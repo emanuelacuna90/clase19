1009a73104cdf4c31a2a997ea8708ec8cde87d4e
emanuelacuna90 <emanuel_LT@hotmail.com>
2020-03-24 23:31:35 -0300

    modificado por Emanuel

---
+++ archivo/archivoComun.js
@@ -0,0 +1,13 @@
+let dia = 'miercoles'
+function finDeSemana (dia) {
+switch (dia) {	
+	case 'sabado':
+    	console.log('buen finde');
+	break;
+	case 'lunes':
+    	console.log('buena semana');
+	break;
+    	default:
+		 console.log('estamos en cuarentena, quiero salir de mi casa!!!!');
+	}
+}
